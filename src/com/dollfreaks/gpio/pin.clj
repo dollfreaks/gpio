b3599890c78e8942ea111ad9f227b5fbe32955e3
Darrell Hamilton <darrell.noice@gmail.com>
2013-09-24 23:26:21 -0700

    further filling out the gpio sysfs api

--- src/com/dollfreaks/gpio/pin.clj
+++ src/com/dollfreaks/gpio/pin.clj
@@ -1,21 +1,47 @@
-(ns com.dollfreaks.gpio.pin)
+(ns com.dollfreaks.gpio.pin
+  (:require [clojure.java.io :refer (file)]))
 
 (let [gpio-dir "/sys/class/gpio"
-      export-file (str gpio-dir "/export")
-      unexport-file (str gpio-dir "/unexport")
-      STATES {:high "1" :low "0"}]
-  (defn- pin-file [p] (str gpio-dir "/gpio" p))
-  (defn- value-file [p] (str (pin-file p) "/value"))
+      export-file (file gpio-dir "export")
+      unexport-file (file gpio-dir "unexport")
+      STATES {:high "1" :low "0"}
+      DIRECTIONS {:input "in" :output "out"}
+      INTERRUPTS {:none "none" :rising "rising" :falling "falling" :all "both"}
+      pin-dir (memoize (fn [p] (file (str gpio-dir "/gpio" p))))
+      pin-file (memoize (fn [p f] (file (pin-dir p) (str "/" f))))
+      pin-file-fn (fn [f] (fn [p] (pin-file p f)))
+      value-file (pin-file-fn "value")
+      direction-file (pin-file-fn "direction")
+      edge-file (pin-file-fn "edge")
+      active-low-file (pin-file-fn "active_low")]
 
   (defn export! [p] (spit export-file p))
   (defn unexport! [p] (spit unexport-file p))
+  (defn exported? [p] (.exists (pin-dir p)))
 
-  (defn exported? [p] (.exists (clojure.java.io/file (pin-file p))))
+  (defn set-direction! [p dir]
+    (let [f (direction-file p)]
+      (if (.exists f)
+        (spit f (get (merge DIRECTIONS STATES dir)))
+        (throw (Exception. (str "Cannot change direction for GPIO pin " p))))))
 
-  (defn direction [p])
+  (defn input! [p] (set-direction! p :input))
+
+  (defn output!
+    "Set the pin direction as output, optionally setting the default pin value"
+    ([p] (output! p :low))
+    ([p v] (set-direction! p v)))
+
+  (defn direction [p]
+    (case (slurp (direction-file p))
+      (:input DIRECTIONS) :input
+      (:output DIRECTIONS) :output
+      nil))
+
+  (defn input? [p] (= :input (direction p)))
+  (defn output? [p] (= :output (direction p)))
 
   (defn set-state! [p s] (spit (value-file p) (s STATES)))
-
   (defn high! [p] (set-state! p :high))
   (defn low! [p] (set-state! p :low))
 
@@ -25,4 +51,14 @@
                     nil))
 
   (defn high? [p] (= :high (state p)))
-  (defn low? [p] (= :low (state p))))
+  (defn low? [p] (= :low (state p)))
+
+  (defn interrupt-on! [p e]
+    (let [f (edge-file p)]
+      (if (.exists f)
+        (spit f (get INTERRUPTS e))
+        (throw (Exception. (str "Cannot configure interrupts for GPIO pin " p))))))
+
+  (defn active-on! [p v]
+    "Should :low or :high be considered the 'rising' value and vice versa"
+    (spit (active-low-file p) (case v :low 1 :high 0 0))))
