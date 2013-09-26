4b45694f7d11b8b56211342b73bc47308fe51248
Darrell Hamilton <darrell.noice@gmail.com>
2013-09-25 23:03:02 -0700

    filling out the project info a little

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.dollfreaks.gpio "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.dollfreaks/gpio "0.1.0-SNAPSHOT"
+  :description "Linux GPIO sysfs api for clojure"
+  :url "https://github.com/dollfreaks/gpio"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
